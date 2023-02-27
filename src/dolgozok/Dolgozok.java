fcb475d68f6db7e7f384e6fa821c771fcd1f5299
rohacsidana <rohacsi.dana@gmail.com>
2023-02-27 13:11:38 +0100

    gui fájl létrehozás, emberek.txt

--- src/dolgozok/Dolgozok.java
+++ src/dolgozok/Dolgozok.java
@@ -1,12 +1,11 @@
-
 package dolgozok;
 
+import gui.DolgozokGUI;
 
 public class Dolgozok {
 
-    
     public static void main(String[] args) {
-        
+        new DolgozokGUI().setVisible(true);
     }
 
 }
